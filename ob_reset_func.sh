64d3e70c1d50f34c8460533d6fd03c4cbd75190e
silascript <silascript@outlook.com>
2024-01-27 03:46:45 +0800

    修正函数返回值

--- ob_reset_func.sh
+++ ob_reset_func.sh
@@ -6,46 +6,54 @@
 # -------------------------函数定义区------------------------ #
 
 # 检测目录是否存在
+# 返回值为1即不存在，0为存在
 function validate_dir(){
 
-	dir_path=$1
+	local dir_path=$1
 	
 	# 检测路径是否为空
 	if [ -z $dir_path ];then
 		echo -e "\e[96m Vault路径不能为空！\n \e[0m"
-		return 1
+		# return 1
+		echo 1
 	fi
 	
 	# 目录存在
 	if [ ! -d "$dir_path" ];then
-		return 1
+		# return 1
+		echo 1
 	else
-		return 0
+		# return 0
+		echo 0
 	fi
 
 }
 
 
 # 检测库的路径
+# 0 存在
+# 1 不存在
 function validate_VaultPath(){
 
 	# 库的路径
-	vault_root=$1
+	local vault_root=$1
 	
 	# 检测目录
-	validate_dir $vault_root
-	vr=$?
+	# validate_dir $vault_root
+	local vr=$(validate_dir $vault_root)
+	# local vr=$?
 
-	return $vr 
+	# return $vr 
+	echo $vr 
 }
 
 # 检测 Vault 中的.obsidian目录存在
 function validate_vault_configdir(){
 
 	# vault 路径
-	vault_path=$1
+	local vault_path=$1
 	# vault 的配置目录.obsidian
-	configdir_name=".obsidian"
+	local configdir_name=".obsidian"
 
 	# 检测 vault 路径是否是/结尾
 	# 如果没有/结尾，就给它加上
@@ -54,11 +62,15 @@ function validate_vault_configdir(){
 	fi
 
 	# vault 配置目录完整路径
-	configdir_path=$vault_path$configdir_name
+	local configdir_path=$vault_path$configdir_name
 	
 	# echo $configdir_path
 
-	return $(validate_dir $configdir_path)
+	# return $(validate_dir $configdir_path)
+	local v_r=$(validate_dir $configdir_path)
+	# echo $(validate_dir $configdir_path)
+	# return $v_r
+	echo $v_r
 	# cfdir_result=$?
 	
 	# return $cfdir_result
@@ -71,12 +83,12 @@ function validate_vault_configdir(){
 function delete_vault_configdir(){
 
 	# Vault 路径
-	vault_path=$1
+	local vault_path=$1
 
-	vconfig_dir=".obsidian"
+	local vconfig_dir=".obsidian"
 
 	validate_VaultPath $vault_path
-	vault_exists=$?
+	local vault_exists=$?
 
 	if [ $vault_exists != 0 ];then
 		echo -e "\e[92m $vault_path \e[96m不存在，删除失败！\n \e[0m"
@@ -85,7 +97,7 @@ function delete_vault_configdir(){
 	
 	# 检测 Vault 下的 .obsidian 目录是否存在
 	validate_vault_configdir $vault_path
-	vault_configdir_exists=$?
+	local vault_configdir_exists=$?
 
 	# echo -e "\e[93m$exists_result \n \e[0m"
 	# echo $exists_result
@@ -117,11 +129,11 @@ function delete_vault_configdir(){
 function reset(){
 	
 	# Vault 路径
-	vault_path=$1
+	local vault_path=$1
 	
 	# 检测 Vault 路径
 	validate_VaultPath $vault_path
-	vp_result=$?
+	local vp_result=$?
 
 	# echo -e "\e[96m$vp_result \n \e[0m"
 	
@@ -144,16 +156,37 @@ function reset(){
 # validate_dir $1
 # echo $?
 
+# d_path=~/MyNotes/Test_Vault/test01
+# d_path=~/MyNotes/Test_Vault/test02
+# validate_dir $d_path
+# r1=$(validate_dir $d_path)
+# echo $r1
+# echo $?
+
+
+# 测试 validate_VaultPath 函数
+# v_path=~/MyNotes/Test_Vault/test01
+# v_path=~/MyNotes/Test_Vault/test02
+# r2=$(validate_VaultPath $v_path)
+# echo $r2
+
+
 # 检测 Vault 下的 .obsidian
 # validate_vault_configdir $1
 # echo $?
 
+# d_path=~/MyNotes/Test_Vault/test01
+# d_path=~/MyNotes/Test_Vault/test02
+# d_r=$(validate_vault_configdir $d_path)
+# echo $d_r
 
 # 删除
 # delete_vault_configdir $1
 
 # reset $1
 
+# d_path=~/MyNotes/Test_Vault/test01
+# reset $d_path
 
 
 
