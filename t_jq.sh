81c0d5fd85ab32d3306f9b7c8cb85a42ef5f2acf
silascript <silascript@outlook.com>
2024-04-14 01:16:24 +0800

    完善获取地址测试

--- t_jq.sh
+++ t_jq.sh
@@ -7,59 +7,23 @@ function get_dl_url() {
 	# json文件
 	local json_path=$1
 
-	# jq -r '.assets[] | .browser_download_url | select ( contains("main.js") or contains("manifest.json") or contains("styles.css") )' $json_path
-
 	# echo $json_path
 
 	# 从github的releases json文件中获取 main.js manifest.json styles.css的地址
-	# local dl_file_addr=$(curl $json_path | jq -r '.assets[] | .browser_download_url | select ( contains("main.js") or contains("manifest.json") or contains("styles.css") ) ')
-	# 取值并输出成数组
-	local dl_file_addr_arr=$(curl $json_path | jq -r '.assets[] | .browser_download_url | select ( contains("main.js") or contains("manifest.json") or contains("styles.css") ) ')
-	# 将获取到的字符串构建成一个列表
-	# local dl_file_addr_list=($(curl $json_path | jq -r '.assets[] | .browser_download_url | select ( contains("main.js") or contains("manifest.json") or contains("styles.css") )'))
+	local dl_file_addr=$(curl $json_path | jq -r '.assets[] | .browser_download_url | select ( contains("main.js") or contains("manifest.json") or contains("styles.css") ) ')
 
 	# echo $dl_file_addr
 
-	# 判断字符串长度是丰大于0
-	# if [[ -n $dl_file_addr ]]; then
+	# 判断长度
+	# 即是否取到文件地址值
+	if [[ ${#dl_file_addr[@]} -gt 0 ]]; then
 
-	# 判断列表长度是否大于0
-	# 即检测是否取到文件地址
-	# if [[ ${#dl_file_addr_list[@]} -gt 0 ]]; then
+		# 返回
+		echo ${dl_file_addr[@]}
 
-	# 判断数组长度
-	if [[ ${#dl_file_addr_arr[@]} -gt 0 ]]; then
-
-		# 返回数组
-		echo $dl_file_addr_arr
-
-		# 返回列表
-		# echo ${dl_file_addr_list[@]}
-
-		# 将字符串按空格分割成数组
-		# local addr_arr=($dl_file_addr)
-
-		# for addr_temp in ${addr_arr[@]}; do
-		# 	echo $addr_temp
-		# done
-		# 返回数组
-		# echo ${addr_arr[@]}
 	else
-		echo -e "\e[92m 取不到文件地址！\n \e[0m"
+		echo -e "\e[93m 取不到文件地址！\n \e[0m"
 	fi
-
-	# 使用 jq 获取各文件下载地址并输出到临时文件中
-	# jq -r '.assets[] | .browser_download_url' $json_path > temp.txt
-
-	# 过滤掉 main.js manifest.json styles.css 三个文件之外所有文件
-	# for line in `cat temp.txt`
-	# do
-	# local f_name=${line##*/}
-	#
-	# if [[ $f_name == "main.js" ]] || [[ $f_name == "manifest.json" ]] || [[ $f_name == "styles.css" ]];then
-	# echo $line
-	# fi
-	# done
 
 }
 
@@ -71,14 +35,38 @@ function get_plugins_id() {
 
 	if [[ ! -e $json_file ]]; then
 		echo -e "\e[93m $json_file \e[92m文件不存在！\n \e[0m"
+	else
+		# 取文件名
+		local fileName=${json_file##*\/}
+		if [[ $fileName != "manifest.json" ]]; then
+			echo -e "\e[93m $json_file \e[92m不是 manifest.json 文件！\n e[0m"
+		else
+			# 获取 id
+			local idvalue=$(curl $json_file | jq -r '.id')
+			echo $idvalue
+		fi
+
 	fi
 
 }
 
 # -----------------------测试--------------------- #
-dl_arr=$(get_dl_url $1)
-# echo ${dl_arr[@]}
+dl_arr_str=$(get_dl_url $1)
+# get_dl_url $1
 
-for addr_temp in ${dl_arr[@]}; do
-	echo $addr_temp
-done
+# echo ${dl_arr_str[@]}
+# 转成数组
+arr1=($dl_arr_str)
+# echo ${arr1[@]}
+echo ${arr1[1]}
+
+# for addr_temp in ${dl_arr[@]}; do
+# 	echo $addr_temp
+# done
+
+# addr2=${dl_arr[1]}
+# echo $addr2
+
+# json_addr=${dl_arr[1]}
+# echo "文件地址："$json_addr
+# get_plugins_id $json_addr
