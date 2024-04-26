877cba0e7053761ded92f153b2eadfc68b6465fb
silascript <silascript@outlook.com>
2024-04-26 11:44:42 +0800

    完善基本函数脚本

--- linux/ob_base_func.sh
+++ linux/ob_base_func.sh
@@ -12,24 +12,23 @@ function validate_dir() {
 
 	# 检测路径是否为空
 	if [ -z $dir_path ]; then
-		echo -e "\e[93m Vault路径不能为空！\n \e[0m"
+		echo -e "\e[93m 路径不能为空！\n \e[0m"
 		return 1
 	fi
 
 	# 目录存在
 	if [ ! -d "$dir_path" ]; then
-		echo -e "\e[93m $dir_path \e[96mVault路径不存在！\n \e[0m"
+		echo -e "\e[93m $dir_path \e[96m目录路径不存在！\n \e[0m"
 		return 1
 	else
 		# return 0
-		echo 0
+		echo "200"
 	fi
 
 }
 
 # 检测库的路径
-# 0 存在
-# 1 不存在
+# 200 存在 其余不存在
 function validate_vault_path() {
 
 	# 库的路径
@@ -38,19 +37,33 @@ function validate_vault_path() {
 	# 检测目录
 	# validate_dir $vault_root
 	local vr=$(validate_dir $vault_root)
-	# local vr=$?
+	# 如果 Vault 路径检测不通过
+	if [[ $vr != "200" ]]; then
+		echo $vr
+		return 1
+	fi
 
-	# return $vr
+	# 检测通过返回 200
 	echo $vr
 }
 
 # 检测 Vault 中的.obsidian目录存在
+# 参数：Vault 根路径
 function validate_vault_configdir() {
 
-	# vault 路径
+	# vault 根路径
 	local vault_path=$1
 	# vault 的配置目录.obsidian
 	local configdir_name=".obsidian"
+
+	# 检测 Vault 路径
+	local vp_r=$(validate_dir $vault_path)
+
+	# 如果 Vault 路径检测不通过
+	if [[ $vp_r != "200" ]]; then
+		echo $vp_r
+		return 1
+	fi
 
 	# 检测 vault 路径是否是/结尾
 	# 如果没有/结尾，就给它加上
@@ -65,27 +78,25 @@ function validate_vault_configdir() {
 
 	# return $(validate_dir $configdir_path)
 	local v_r=$(validate_dir $configdir_path)
-	# echo $(validate_dir $configdir_path)
-	# return $v_r
+	# 返回检测结果
+	# 如果检测通过返回 200
 	echo $v_r
-	# cfdir_result=$?
-
-	# return $cfdir_result
 
 }
 
 # -------------------------测试区------------------------ #
 
 # 检测 Vault 路径
-r_1=$(validate_dir $1)
+# r_1=$(validate_dir $1)
 # echo $r_1
-echo $?
+# echo $?
 
 # d_path=~/MyNotes/Test_Vault/test01
 # d_path=~/MyNotes/Test_Vault/test02
 # validate_dir $d_path
 # r1=$(validate_dir $d_path)
-# echo $r1
+r1=$(validate_vault_configdir $1)
+echo $r1
 # echo $?
 
 # 测试 validate_vault_path 函数
